f2c2d94132257d5bcc30e199e9f6f28f8423082a
Santosh Balchandran <santoshbalchandran@gmail.com>
2022-12-30 17:08:03 +0530

    Install material UI

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import CustomButton from "./CustomButton";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>This is a App</h2>
+      <CustomButton />
+    </div>
+  );
+}
+
+export default App;
