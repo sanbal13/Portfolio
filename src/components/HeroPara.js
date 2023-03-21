d245f209241e693d85578ad07da66d9bab0cee29
Santosh Balchandran <santoshbalchandran@gmail.com>
2023-03-21 15:22:22 +0530

    Edit hero para and change hover effect in blog section

--- src/components/HeroPara.js
+++ src/components/HeroPara.js
@@ -8,7 +8,7 @@ export default function HeroPara() {
     <span className="circle"></span>
     <span className="circle"></span>
     </div>
-      <p className="hero-para">I'm a Full Stack Web Developer, looking for an opportunity as a Full Stack Developer/ Frontend Developer / Backend Developer. My preference is for React Frontend development. My strengths are logical thinking, problem solving and debugging.</p>
+      <p className="hero-para">I'm a Full Stack Web Developer, looking for an opportunity as a Full Stack Developer/ Frontend Developer / Backend Developer. My preference is for React Frontend development. My strengths are logical thinking, problem solving,debugging and creating impeccable layouts.</p>
       
     </div>
   );
