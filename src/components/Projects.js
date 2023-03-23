e9367e840b14a9e9eb5efeae8077e7a6ad8a55ef
Santosh Balchandran <santoshbalchandran@gmail.com>
2023-03-23 20:08:43 +0530

    Create Basic structure of Project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import projects from '../data/projects'
 
 export default function Projects() {
   return (
@@ -6,21 +7,18 @@ export default function Projects() {
     <div className="container">
     <h2>Projects</h2>
      <ul className='flex'>     
-        <li className='project-card'><a href="https://activity-tracker-tau.vercel.app/" target="_blank" rel="noreferrer"><img src="./images/activity_tracker.png" alt="activity_tracker" className="thumbnail"/></a>
-        <h3>Project Name: Activity Tracker</h3>        
-        <h3>Github Link: <a href="https://github.com/sanbal13/activity-tracker">https://github.com/sanbal13/activity-tracker</a></h3>
-        <a href="https://activity-tracker-tau.vercel.app/" target="_blank" rel="noreferrer" className="site-button">Visit Site</a>
-        </li>   
-        <li className='project-card'><a href="https://calculator-swart-seven.vercel.app/" target="_blank" rel="noreferrer"><img src="./images/calculator.png" alt="calculator" className="thumbnail"/></a>
-        <h3>Project Name: Calculator</h3>        
-        <h3>Github Link: <a href="https://github.com/sanbal13/Calculator">https://github.com/sanbal13/Calculator</a></h3>
-        <a href="https://calculator-swart-seven.vercel.app/" target="_blank" rel="noreferrer" className="site-button">Visit Site</a>
-        </li>   
-        <li className='project-card'><a href="https://logic-games.vercel.app/" target="_blank" rel="noreferrer"><img src="./images/logic_games.png" alt="logic games" className="thumbnail"/></a>
-        <h3>Project Name: Logic Games</h3>        
-        <h3>Github Link: <a href="https://github.com/sanbal13/Logic-Games">https://github.com/sanbal13/Logic-Games</a></h3>
-        <a href="https://logic-games.vercel.app/" target="_blank" rel="noreferrer" className="site-button">Visit Site</a>
-        </li>            
+        {projects.map(project => (
+          <li key={project.id}>
+          <h3 className="project-title">{project.title}</h3>
+          <div className='card'>
+          <img src={project.image} alt={project.title} className='thumbnail'/>
+          <a href={project.link} target='_blank' rel='noreferrer' className='card-button'>Visit Site</a>
+          <a href={project.github_link} target='_blank' rel='noreferrer' className='github-link'>GithubLink</a>
+          </div>
+          </li>
+        ))}
+       
+                
      </ul>
      </div>
      </div>
---
+++ src/data/projects.js
@@ -0,0 +1,24 @@
+const projects = [
+    {
+        id: 1,
+        title: "Activity Tracker",
+        link: "https://activity-tracker-tau.vercel.app/",
+        image: "./images/projects/activity_tracker.png",
+        github_link: "https://github.com/sanbal13/activity-tracker"
+    },
+    {
+        id: 2,
+        title: "Calculator",
+        link: "https://calculator-swart-seven.vercel.app/",
+        image: "./images/projects/calculator.png",
+        github_link: "https://github.com/sanbal13/Calculator"
+    },
+    {
+        id: 3,
+        title: "Logic Games",
+        link: "https://logic-games.vercel.app/",
+        image: "./images/projects/logic_games.png" ,
+        github_link: "https://github.com/sanbal13/Logic-Games"
+    }
+];
+export default projects;
