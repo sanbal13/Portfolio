94286480fd51f93136e5db0737a76460d7717aaf
Santosh Balchandran <santoshbalchandran@gmail.com>
2023-03-28 00:53:31 +0530

    Update Hero Section Para

--- src/components/HeroPara.js
+++ src/components/HeroPara.js
@@ -8,7 +8,9 @@ export default function HeroPara() {
     <span className="circle"></span>
     <span className="circle"></span>
     </div>
-      <p className="hero-para">A Full Stack Web Developer, looking for opportunities as a Full Stack Developer/ Frontend Developer / Backend Developer. <br /><h5>Preference</h5> React Frontend development. <br /><h5>Strengths</h5> Logical thinking, problem solving,debugging and creating impeccable layouts.</p>      
+      <p className="hero-para">A Full Stack Web Developer, looking for opportunities as a Full Stack Developer/ Frontend Developer / Backend Developer. <br />
+      <span className='hero-title'>Preference</span> React Frontend development. <br />
+      <span className='hero-title'>Strengths</span> Logical thinking, problem solving,debugging and creating impeccable layouts.</p>      
     </div>
   );
 }
