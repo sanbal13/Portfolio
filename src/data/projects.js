8057b68e8ea21cf1a4f96a4cc8e0d782a195b817
Santosh Balchandran <santoshbalchandran@gmail.com>
2023-03-28 00:54:37 +0530

    Update the Projects data file

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,21 +4,24 @@ const projects = [
         title: "Activity Tracker",
         link: "https://activity-tracker-tau.vercel.app/",
         image: "./images/projects/activity_tracker.png",
-        github_link: "https://github.com/sanbal13/activity-tracker"
+        github_link: "https://github.com/sanbal13/activity-tracker",
+        type: "react"
     },
     {
         id: 2,
         title: "Calculator",
         link: "https://calculator-swart-seven.vercel.app/",
         image: "./images/projects/calculator.png",
-        github_link: "https://github.com/sanbal13/Calculator"
+        github_link: "https://github.com/sanbal13/Calculator",
+        type: "hobby"
     },
     {
         id: 3,
         title: "Logic Games",
         link: "https://logic-games.vercel.app/",
         image: "./images/projects/logic_games.png" ,
-        github_link: "https://github.com/sanbal13/Logic-Games"
+        github_link: "https://github.com/sanbal13/Logic-Games",
+        type: 'hobby'
     }
 ];
 export default projects;
